fc2200bd10d28ebea7d0a2eb05115bee577e10c4
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-21 21:18:41 -0300

    linhas adicionadas

--- praticando_arquivos/Program.cs
+++ praticando_arquivos/Program.cs
@@ -2,6 +2,20 @@
 {
     public static void Main(string[] args)
     {
+        string pathData = @"/Users/rodrigo/Desktop/CSharp/praticando_arquivos/data/brasileirao-2004-2022.csv";
+        List<string[]> data = new();
 
+        if (File.Exists(pathData))
+        {
+            string[] dataVetor = File.ReadAllLines(pathData);
+
+            foreach (string dt in dataVetor)
+            {
+                string[] lines = dt.Split(", ");
+                data.Add(lines);
+            }
+        }
+
+        Console.ReadKey();
     }
 }
